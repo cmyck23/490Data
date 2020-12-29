71b91926b4c26f496a0ac216c0dd0f24f2523c1d
Walther Myckael <myckael24@gmail.com>
2020-12-29 15:17:35 -0500

    Combined accelerometer and gps  coordinates

--- roadData/ViewController.swift
+++ roadData/ViewController.swift
@@ -7,13 +7,63 @@
 
 import UIKit
 import CoreMotion
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
     var motionManager = CMMotionManager()
+    
+    // Used to start getting the users location
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // For use when the app is open & in the background
+                locationManager.requestAlwaysAuthorization()
+                
+                // For use when the app is open
+                //locationManager.requestWhenInUseAuthorization()
+                
+                // If location services is enabled get the users location
+                if CLLocationManager.locationServicesEnabled() {
+                    locationManager.delegate = self
+                    locationManager.desiredAccuracy = kCLLocationAccuracyBest // You can change the locaiton accuary here.
+                    locationManager.startUpdatingLocation()
+                }
+            }
+            
+            // Print out the location to the console
+            func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+                if let location = locations.first {
+                    print(location.coordinate)
+                }
+            }
+            
+            // If we have been deined access give the user the option to change it
+            func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+                if(status == CLAuthorizationStatus.denied) {
+                    showLocationDisabledPopUp()
+                }
+            }
+            
+            // Show the popup to the user if we have been denied access
+            func showLocationDisabledPopUp() {
+                let alertController = UIAlertController(title: "Background Location Access Disabled",
+                                                        message: "In order to deliver pizza we need your location",
+                                                        preferredStyle: .alert)
+                
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                alertController.addAction(cancelAction)
+                
+                let openAction = UIAlertAction(title: "Open Settings", style: .default) { (action) in
+                    if let url = URL(string: UIApplication.openSettingsURLString) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    }
+                }
+                alertController.addAction(openAction)
+                
+                self.present(alertController, animated: true, completion: nil)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -26,7 +76,7 @@ class ViewController: UIViewController {
             {
                 if myData.acceleration.x > 0.5
                 {
-                    print ("DO SOMETHING SPCIAL")
+                    print ("Displaying acceleromter data")
                     print( myData.acceleration.x)
                     print( myData.acceleration.y)
                     print( myData.acceleration.z)
